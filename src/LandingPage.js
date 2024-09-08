419dc545f406ab1eb099c3fdd4bdae11c6d12dd9
udaybuilds <udaysrivastava0@gmail.com>
2024-09-09 01:37:20 +0530

    Adding correct Url

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { GithubIcon, LinkedinIcon, MailIcon } from 'lucide-react';
+import { GithubIcon, LinkedinIcon, MailIcon , ChartSpline } from 'lucide-react';
+
 
 const LandingPage = () => {
   return (
@@ -31,18 +32,24 @@ const LandingPage = () => {
           </ul>
         </section>
 
-        <section>
+        <section className="mb-16">
           <h2 className="text-3xl font-semibold mb-6">Featured Projects</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div className="bg-white p-8 rounded-lg shadow-lg transform transition-transform hover:scale-105">
-              <h3 className="text-2xl font-semibold mb-3">Project 1</h3>
-              <p>A brief description of your first project goes here.</p>
+              <h3 className="text-2xl font-semibold mb-3">Note Keeper</h3>
+              <p><a href="https://udaybuilds.github.io/Keeper-React-App/" style={{ textDecoration: 'none', color: 'Blue' }}>Note Keeper Application</a></p>
             </div>
             <div className="bg-white p-8 rounded-lg shadow-lg transform transition-transform hover:scale-105">
-              <h3 className="text-2xl font-semibold mb-3">Project 2</h3>
-              <p>A brief description of your second project goes here.</p>
+              <h3 className="text-2xl font-semibold mb-3">Youtube Clone Hosted on Azure App Services</h3>
+              <p><a href="https://youtubebyuday.azurewebsites.net/" style={{ textDecoration: 'blink', color: 'Blue' }}>Youtube Clone maintained on Azure DevOps</a></p>
             </div>
           </div>
+        </section>
+        <section className="mb-16">
+          <h2 className="text-3xl font-semibold mb-6">Resume</h2>
+          <p className="text-xl leading-relaxed">
+            <a href="./UDAY_RESUME.PDF">Resume</a>
+          </p>
         </section>
       </main>
 
@@ -58,6 +65,9 @@ const LandingPage = () => {
             </a>
             <a href="https://mail.google.com/mail/#inbox?compose=new" className="hover:text-gray-400 transform transition-transform hover:scale-110">
               <MailIcon size={24} />
+            </a>
+            <a href="https://leetcode.com/u/udaysrivastava0/" className="hover:text-gray-400 transform transition-transform hover:scale-110">
+              <ChartSpline size={24} />
             </a>
           </div>
         </div>
